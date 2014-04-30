b8a6c6f535f52e483f797d71727da5486e33f976
bren525 <bren525@gmail.com>
2014-04-29 21:22:29 -0700

    added thread mlfqs update

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -675,7 +675,7 @@ allocate_tid (void)
 }
 
 void thread_mlfqs_update(struct thread *t, void *aux){
-  thread_set_priority(PRI_MAX - (thread_get_recent_cpu()/4) - (t->nice*2));
+  thread_set_priority(PRI_MAX - (other_thread_get_recent_cpu(t)/4) - (t->nice*2));
 }
 
 /* Offset of `stack' member within `struct thread'.
